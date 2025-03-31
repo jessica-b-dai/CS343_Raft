c37b9aa60290d2a294749f423274fc2c98326144
Jessica Dai <jessica.b.dai@gmail.com>
2025-03-30 21:07:33 -0400

    got general logic complete

--- raftNode.go
+++ raftNode.go
@@ -4,10 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"net/rpc"
 	"os"
 	"strconv"
+	"sync"
+	"time"
 )
 
 type RaftNode int
@@ -41,32 +44,155 @@ type ServerConnection struct {
 var selfID int
 var serverNodes []ServerConnection
 var currentTerm int
-var votedFor int
+var votedFor int = -1
+var electionTimer *time.Timer
+var mutex sync.Mutex
+var state string = "follower"
 
 // The RequestVote RPC as defined in Raft
 // Hint 1: Use the description in Figure 2 of the paper
 // Hint 2: Only focus on the details related to leader election and majority votes
-func (*RaftNode) RequestVote(arguments VoteArguments, reply *VoteReply) error {
+func (node *RaftNode) RequestVote(arguments VoteArguments, reply *VoteReply) error {
+	mutex.Lock()
+	defer mutex.Unlock()
 
+	fmt.Println("Received vote request from", arguments.CandidateID, "|| term:", arguments.Term)
+
+	// If the requester's term is higher than our own, we increment our own term and vote yes.
+	if arguments.Term > currentTerm {
+		currentTerm = arguments.Term // update own term
+		votedFor = -1                // New term, so initializing votedFor to be -1
+		// Log recency check here - not implemented
+		// May want to choose a different leader
+		votedFor = arguments.CandidateID
+		reply.ResultVote = true
+		reply.Term = arguments.Term
+		state = "follower" // change our state to follower, in case we were a leader that blipped and came back
+		fmt.Println("Voted for", votedFor)
+		node.resetElectionTimer()
+	} else { // If the requester's term is less than our own, automatic reject.
+		reply.ResultVote = false
+		reply.Term = currentTerm
+	}
 	return nil
 }
 
 // The AppendEntry RPC as defined in Raft
 // Hint 1: Use the description in Figure 2 of the paper
 // Hint 2: Only focus on the details related to leader election and heartbeats
-func (*RaftNode) AppendEntry(arguments AppendEntryArgument, reply *AppendEntryReply) error {
-
+func (node *RaftNode) AppendEntry(arguments AppendEntryArgument, reply *AppendEntryReply) error {
+	fmt.Println("Received Heartbeat from", arguments.LeaderID, "|| term:", arguments.Term)
+	mutex.Lock()
+	defer mutex.Unlock()
+	if arguments.Term < currentTerm { // not our leader, leader of lower term.
+		reply.Success = false
+		reply.Term = currentTerm
+	} else {
+		fmt.Println("Responded to", arguments.LeaderID)
+		// respond to our leader
+		reply.Success = true
+		currentTerm = arguments.Term
+		reply.Term = currentTerm
+		// reset timer
+		node.resetElectionTimer()
+	}
 	return nil
+}
+
+func (node *RaftNode) resetElectionTimer() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if electionTimer != nil {
+		electionTimer.Stop() // stops existing timer so that only one is running at a time
+	}
+
+	// min := 150 * time.Millisecond
+	// max := 300 * time.Millisecond
+	min := 5 * time.Second
+	max := 10 * time.Second
+	timerLength := time.Duration(rand.Int63n(max.Nanoseconds()-min.Nanoseconds()) + min.Nanoseconds())
+	electionTimer = time.AfterFunc(timerLength, func() {
+		fmt.Println("Timer Expired. Starting Leader Election ...")
+		node.LeaderElection() // start leader election if timer runs out
+	})
 }
 
 // You may use this function to help with handling the election time out
 // Hint: It may be helpful to call this method every time the node wants to start an election
-func LeaderElection() {
+func (node *RaftNode) LeaderElection() {
+	fmt.Println("Starting leader election from node ", selfID)
+	node.resetElectionTimer() // set timer for election should be done - allows election to restart if vote is split
+
+	mutex.Lock()
+	// increment its current term, turn self into candidate
+	currentTerm++
+	state = "candidate"
+	votedFor = selfID // vote for self
+	numVotes := 1
+	mutex.Unlock()
+
+	// Issues RequestVote RPCS to all other servers
+	for _, server := range serverNodes {
+		arguments := VoteArguments{Term: currentTerm, CandidateID: selfID}
+
+		fmt.Println("Requesting vote from ", server.serverID, server.Address)
+		go func(server ServerConnection) {
+			reply := VoteReply{}
+			err := server.rpcConnection.Call("RaftNode.RequestVote", arguments, &reply)
+			if err != nil {
+				return
+			}
+
+			// Take result of the RPC call, and increment number of votes accordingly
+			if reply.ResultVote {
+				mutex.Lock()
+				numVotes++
+				// check if there are enough votes for a majority
+				if 2*numVotes > len(serverNodes) && state == "candidate" { //majority reached
+					// successfully elected as leader
+					state = "leader"
+					fmt.Println("Successfully elected as leader: ", selfID)
+					electionTimer.Stop() // cancel timer, not a split vote, got majority
+					go node.Heartbeat()  // starts heartbeats, runs forever
+				}
+				mutex.Unlock()
+			}
+		}(server)
+	}
 }
 
 // You may use this function to help with handling the periodic heartbeats
 // Hint: Use this only if the node is a leader
-func Heartbeat() {
+func (node *RaftNode) Heartbeat() {
+	// heartbeatInterval := 50 * time.Millisecond
+	heartbeatInterval := 2 * time.Second
+	for {
+		// make sure we are a leader still, if this state has changed, stop sending heartbeats
+		if state != "leader" {
+			return
+		}
+		//Send heartbeats to all servers
+		for _, server := range serverNodes {
+			arguments := AppendEntryArgument{Term: currentTerm, LeaderID: selfID}
+
+			fmt.Println("Sending heartbeat to ", server.serverID, server.Address)
+			go func(server ServerConnection) {
+				reply := AppendEntryReply{}
+				err := server.rpcConnection.Call("RaftNode.AppendEntry", arguments, &reply)
+				if err != nil {
+					return
+				}
+
+				// Step down as leader if it seems that other nodes have a higher term
+				if reply.Term > currentTerm {
+					state = "follower"
+				}
+			}(server)
+		}
+		timer := time.NewTimer(heartbeatInterval)
+		<-timer.C
+	}
 }
 
 func main() {
@@ -82,6 +208,9 @@ func main() {
 
 	// Get this sever's ID (same as its index for simplicity)
 	myID, err := strconv.Atoi(arguments[1])
+	if err != nil {
+		log.Fatal(err)
+	}
 	// Get the information of the cluster configuration file containing information on other servers
 	file, err := os.Open(arguments[2])
 	if err != nil {
@@ -164,5 +293,9 @@ func main() {
 	// Hint 3: After this point, the threads should take over
 	// Heads up: they never will be done!
 	// Hint 4: wg.Wait() might be helpful here
-
+	var wg sync.WaitGroup
+	wg.Add(1)
+	selfID = myID
+	api.resetElectionTimer()
+	wg.Wait() // Waits forever, so main process does not stop
 }
